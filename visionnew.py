426a9c93e96e9c2c7f7de06a78fa2df717f8bb3a
Mark Cook <mkcmobile@gmail.com>
2017-02-17 08:01:28 -0500

    Beginning series of tasks to split high/low target processing due to the difference in vision solutions required (Low target occlusion highly likely at the beginning of autopilot.  This commit splits the processing as-is currently and has been tested with recorded video.

--- visionnew.py
+++ visionnew.py
@@ -135,8 +135,9 @@ send_sock=initUdp('10.31.40.42',5803)	# initializes UDP socket to send to RobioR
 
 targetHigh = {
 	'NumRects' : 2,
-	'Rects' : [[14.0,4.0],[14.0,2.0]], #inches width x height for both rectangles
-	'RectSep' : [0.0,7.0], #inches width, height in separation between rectangle centers
+#	'Rects' : [[14.0,4.0],[14.0,2.0]], #inches width x height for both rectangles
+	'Rects' : [[14.0,4.0],[14.0,2.0]], #test target that is wrong dimensions
+	'RectSep' : [0.0,5.0], #inches width, height in separation between rectangle centers
 	'RectIntensity' : [True,True], #each rectangle should be brighter than surrounding
 	'RectSepTol' : 0.25, #inches tolernce between true and found differences
 	'RectOrient' : 0,  #degrees ideal from horizontal
@@ -187,9 +188,171 @@ def selectTarget (targetSought = 0) :
 		resY = resYLow
 	return (resX,resY,xSize,ySize,camera,target)
 
-resX, resY, xSize, ySize, camera, target = selectTarget(0)
+targetSought = 0		# 0 = Boiler or "High" target; 1 = Peg/Gear or "Low" target
+resX, resY, xSize, ySize, camera, target = selectTarget(targetSought)
 
-def processFrame():			# This function does all of the image processing on a single frame
+def highTargetProcess():
+	boxes = []	#list of best fit boxes to contours
+	boxCenters = [[]]  #centers of boxes
+	ret, frame = camera.read()
+	thresh = 0
+	found = False
+	segments1 = []		# found segment array for rectangle 1
+	segments2 = []		# found segment array for rectangle 2
+	aimPoint = []		# empty until found
+	slantRange = -1.0	# negative means not set
+	bearing = 1.e6		# nonsense until set
+
+	if ret==True:
+		img2 = frame[:,:,1] # green band used only as we are using green LED illuminators
+		ret,thresh = cv2.threshold(img2,imageBinaryThresh,255,cv2.THRESH_BINARY)	# get a binary image of only the brightest areas
+		img2 = thresh.copy()
+		im2, contours, hierarchy = cv2.findContours(img2,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+		for cnt in contours:
+			rect = cv2.minAreaRect(cnt)  #minumum bounding rectangle of the contour
+			box = cv2.boxPoints(rect) #best fit box (rotated) to the shape
+			
+			centerX, centerY = rect[0]
+
+			topLeft = box[0]	# make certain always has a value
+			topRight = box[0]
+			botLeft = box[0]
+			botRight = box[0]
+			for i in range(1,4):
+				if (box[i][0] < centerX) and (box[i][1] < centerY): topLeft = box[i]
+				if (box[i][0] < centerX) and (box[i][1] > centerY): botLeft = box[i]
+				if (box[i][0] > centerX) and (box[i][1] < centerY): topRight = box[i]
+				if (box[i][0] > centerX) and (box[i][1] > centerY): botRight = box[i]
+
+			edgeTop = [(topLeft[0]+topRight[0])/2, (topLeft[1]+topRight[1])/2]
+			edgeBot = [(botLeft[0]+botRight[0])/2, (botLeft[1]+botRight[1])/2]
+			edgeLeft = [(topLeft[0]+botLeft[0])/2, (topLeft[1]+botLeft[1])/2]
+			edgeRight = [(topRight[0]+botRight[0])/2, (topRight[1]+botRight[1])/2]
+
+			width = edgeRight[0] - edgeLeft[0]
+			height = edgeBot[1] - edgeTop[1]
+
+			rect = ((centerX,centerY),(width,height), 0.)
+			box = np.int0(box)
+			# is it a big enough box?
+
+			if cv2.contourArea(box) >= minBoxArea:
+				if (cv2.contourArea(cnt) != 0): 
+					# does it look like a rectangle?
+					if (cv2.contourArea(box)/cv2.contourArea(cnt)) <= areaRatio: 
+						# does it have the right orientation? 
+						centerX, centerY = rect[0]
+						width, height = rect[1]
+						if (height > 0):
+							# First box
+							targetWidth, targetHeight = target['Rects'][0]
+							targetAspectRatio = targetWidth/targetHeight
+							rectAspectRatio = width/height
+							errorAspect = (rectAspectRatio-targetAspectRatio)/targetAspectRatio
+							if (abs(errorAspect) <= aspectRatioTol):
+								segments1.append(rect) #collect the boxes for later processing
+								if args.debug==True: 
+									cv2.drawContours(frame,[box], 0, 255, 2)
+							# Second box								
+							targetWidth, targetHeight = target['Rects'][1]
+							targetAspectRatio = targetWidth/targetHeight
+							rectAspectRatio = width/height
+							errorAspect = (rectAspectRatio-targetAspectRatio)/targetAspectRatio
+							if (abs(errorAspect) <= aspectRatioTol):
+								segments2.append(rect) #collect the boxes for later processing
+								if args.debug==True: 
+									cv2.drawContours(frame,[box], 0, (0,0,255), 2)
+
+
+	if (len(segments1) > 0) and (len(segments2) > 0):			# any candidate pairs?
+		# let's see if the ratios we found are consistent between all the segments
+		# at a given range, all target segment ratios should match all segment ratios relative to each other
+
+		target1Width, target1Height = target['Rects'][0]
+		target2Width, target2Height = target['Rects'][1]
+
+		targetWidthRatio = target1Width / target2Width			
+		targetHeightRatio = target1Height / target2Height
+
+		for rect1 in segments1:
+			width1, height1 = rect1[1]
+
+			for rect2 in segments2:	
+				width2, height2 = rect2[1]
+				heightRatio = height1 / height2
+				heightErrorAspect = (heightRatio - targetHeightRatio) / targetHeightRatio
+
+				if (abs(heightErrorAspect) <= aspectRatioTol):
+					widthRatio = width1 / width2
+					widthErrorAspect = (widthRatio - targetWidthRatio) / targetWidthRatio
+
+					if (abs(widthErrorAspect) <= aspectRatioTol):
+						# each segment appears the right size relative to each other, how about the expected 
+						# separation relative to each other?  Does that match as well on the segments?
+
+						targetSepX, targetSepY = target['RectSep']
+						resApparentX = target1Width / width1			# if top segment is true, this estimates inches/pixel
+						resApparentY = target1Height / height1			# if top segment is true, this estimates inches/pixel
+						targetSepXPixels = targetSepX / resApparentX
+						targetSepYPixels = targetSepY / resApparentY
+						center1X, center1Y = rect1[0]
+						center2X, center2Y = rect2[0]
+						segmentSepX = center2X - center1X				# row, col coordinate system with top left the origin
+						segmentSepY = center2Y - center1Y
+						sepXError = (segmentSepX - targetSepXPixels)
+						sepYError = (segmentSepY - targetSepYPixels)
+
+						if ((abs(sepXError) <= sepPixelTol) and (abs(sepYError) <= sepPixelTol)):
+							found = True
+							minX = 1.e6
+							minY = 1.e6
+							maxX = -1
+							maxY = -1
+
+							box = cv2.boxPoints(rect1)
+							for i in range(0,4):
+								if (box[i][0] < minX): minX = box[i][0]
+								if (box[i][0] > maxX): maxX = box[i][0]
+								if (box[i][1] < minY): minY = box[i][1]
+								if (box[i][1] > maxY): maxY = box[i][1]
+
+							box = cv2.boxPoints(rect2)
+							for i in range(0,4):
+								if (box[i][0] < minX): minX = box[i][0]
+								if (box[i][0] > maxX): maxX = box[i][0]
+								if (box[i][1] < minY): minY = box[i][1]
+								if (box[i][1] > maxY): maxY = box[i][1]
+
+							foundBox = [[minX,minY],
+										[maxX,minY],
+										[maxX,maxY],
+										[minX,maxY]]	
+							
+# Consider change range calculation for boiler target to use the known Target Height location
+# on the funnel for range esimate rather than image extents.
+							
+							targetTotalHeight = (target1Height + target2Height + targetSepY ) / 12.0
+							targetAngle = ((maxY-minY)/ySize) * fovY
+							slantRange = (targetTotalHeight/2.0) / math.tan(targetAngle/2.0)
+							aimPoint = [minX + (maxX-minX)/2.0, minY + (maxY-minY)/2.0]
+							bearing = (aimPoint[0] - xSize/2.0) * math.degrees(resX)
+							foundBox = np.array(foundBox, dtype=np.int32)
+
+							if args.debug==True: 
+								cv2.drawContours(frame,[foundBox], 0, (255,255,0), 2)
+								apX = np.int0(aimPoint[0])	
+								apY = np.int0(aimPoint[1])
+								aimLineStart = (apX-10,apY)
+								aimLineStop  = (apX+10,apY)
+								cv2.line(frame,aimLineStart,aimLineStop,(0,255,255),3)
+								aimLineStart = (apX,apY-10)
+								aimLineStop  = (apX,apY+10)							
+								cv2.line(frame,aimLineStart,aimLineStop,(0,255,255),3)
+
+	return (ret, thresh, frame, found, aimPoint, slantRange, bearing)
+
+def lowTargetProcess():
 
 	boxes = []	#list of best fit boxes to contours
 	boxCenters = [[]]  #centers of boxes
@@ -348,6 +511,16 @@ def processFrame():			# This function does all of the image processing on a sing
 								aimLineStart = (apX,apY-10)
 								aimLineStop  = (apX,apY+10)							
 								cv2.line(frame,aimLineStart,aimLineStop,(0,255,255),3)
+
+	return (ret, thresh, frame, found, aimPoint, slantRange, bearing)
+
+
+def processFrame():			# This function does all of the image processing on a single frame
+
+	if(targetSought==0):
+		ret, thresh, frame, found, aimPoint, slantRange, bearing = highTargetProcess()
+	else:
+		ret, thresh, frame, found, aimPoint, slantRange, bearing = lowTargetProcess()
 
 	return (ret, thresh, frame, found, aimPoint, slantRange, bearing)
 
